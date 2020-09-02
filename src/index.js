54d19de42468015883cd42540300d52028c60e24
Marwa Ragheb <66853095+Marwa7246@users.noreply.github.com>
2020-09-02 15:33:59 -0400

    create useEffect

--- src/index.js
+++ src/index.js
@@ -7,7 +7,19 @@ import Broken from "./components/Broken";
 import Fixed from "./components/Fixed";
 
 function Application() {
-  const [likes, setLikes] = useState(0);
+  const [likes, setLikes] = useState(100);
+
+  useEffect(() => {
+    // if (likes >2) console.log(`Likes count ${likes}`);
+
+    document.body.className = `bg--${likes % 2}`;
+
+    if (likes > 0) {
+      const timeout = setTimeout(() => setLikes((prev) => prev - 1), 1000);
+      console.log(`Likes count ${likes}`);
+      return () => clearInterval(timeout);
+    }
+  }, [likes]);
 
   return (
     <div onClick={() => setLikes(likes + 1)}>
